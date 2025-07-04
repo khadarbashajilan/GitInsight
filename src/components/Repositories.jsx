67fafffea3e11b871c52b874625b612f48d3f72f
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-07-04 19:16:43 +0530

    styling accomplished

--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 // import data from '../repos.json'
 // import code from "./assets/code.svg"
-// const Repositories = () => {
+// const Repos = () => {
 //   const obj = data.map((item) => ({
 //   name: item?.name ?? "-",
 //   description: item?.description ? item.description.length > 100 ? `${item.description.substring(0, 100).trim()} ...`: item.description.trim()
@@ -13,13 +13,15 @@
 //   console.log(obj);
 
 //   return (
-//     <div className='h-screen w-full  flex flex-col gap-5 items-center text-amber-50 '>
-//       <h1 className='self-start ms-3 font-bold text-3xl'>Repositories</h1><br />
-//       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  m-5'>
+//     <div className='h-screen w-full  flex flex-col gap-2 items-center'>
+//       <div className='flex flex-col my-8 mx-5'>
+//       <h1 className='font-bold text-3xl text-center text-white'>Repositories</h1>
+//       </div>
+//       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  m-5 mt-2'>
 //       {
 //         obj.map((item,idx)=>{
 //           return(
-//             <div key={idx} className='hover:border-blue-500 transition-2s border-2 gap-2 p-2 px-4 justify-evenly m-2 my-3 h-65 w-50 md:h-75 md:w-60 lg:h-80 rounded-3xl flex flex-col'>
+//             <div key={idx} className='hover:border-blue-500 hover:transition-2s border-2 gap-2 p-2 px-4 justify-evenly m-2 my-3 h-65 w-50 md:h-75 md:w-60 lg:h-80 rounded-3xl flex flex-col'>
 //             <h2 className='self-start'>{item.name}</h2>
 //             <p className='h-16  my-1 text-[12px] md:text-[14px]  font-extralight'>{item.description}</p>
 //             <div className='flex gap-2 items-center '>
@@ -27,7 +29,7 @@
 //             <h2 className='text-sm'>{item.language}</h2>
 //             </div>
 //             <h2 className='text-sm'>Last updated: {item.updated}</h2>
-//             <button className='cursor-pointer border rounded-2xl p-1 m-2' onClick={() => window.open(item.link, "_blank")}>View Repo</button>
+//             <button className='hover:border-blue-500 hover:transition-2s cursor-pointer border rounded-2xl p-1 m-2' onClick={() => window.open(item.link, "_blank")}>View Repo</button>
 //             </div>
 //           )
 //         })
@@ -37,4 +39,4 @@
 //   )
 // }
 
-// export default Repositories
+// export default Repos
